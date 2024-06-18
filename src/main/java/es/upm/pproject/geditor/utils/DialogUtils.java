f1237dc7a72d4ab75afb249fb92699986832d296
200002 <jorge.gplaza@alumnos.upm.es>
2024-06-18 20:05:01 +0200

    Fix some bugs

--- src/main/java/es/upm/pproject/geditor/utils/DialogUtils.java
+++ src/main/java/es/upm/pproject/geditor/utils/DialogUtils.java
@@ -11,6 +11,17 @@ public class DialogUtils {
     private DialogUtils() {
         throw new UnsupportedOperationException("Utility class");
     }
+    private static final String X_LABEL = "X:";
+    private static final String Y_LABEL = "Y:";
+    private static final String WIDTH_LABEL = "Width:";
+    private static final String HEIGHT_LABEL = "Height:";
+    private static final String CENTER_X_LABEL = "Center X:";
+    private static final String CENTER_Y_LABEL = "Center Y:";
+    private static final String RADIUS_LABEL = "Radius:";
+    private static final String START_X_LABEL = "Start X:";
+    private static final String START_Y_LABEL = "Start Y:";
+    private static final String END_X_LABEL = "End X:";
+    private static final String END_Y_LABEL = "End Y:";
 
     private static SVGElement showShapeDialog(Component parent, String title, JPanel panel, String shapeType) {
         int result = JOptionPane.showConfirmDialog(parent, panel, title, JOptionPane.OK_CANCEL_OPTION,
@@ -23,54 +34,83 @@ public class DialogUtils {
 
     private static SVGElement createSVGElement(JPanel panel, String shapeType) {
         Component[] components = panel.getComponents();
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+        int radius = 0;
+        int centerX = 0;
+        int centerY = 0;
+        int startX = 0;
+        int startY = 0;
+        int endX = 0;
+        int endY = 0;
 
-        switch (shapeType) {
-            case "Rectangle":
-                int x = Integer.parseInt(((JTextField) components[1]).getText());
-                int y = Integer.parseInt(((JTextField) components[3]).getText());
-                int width = Integer.parseInt(((JTextField) components[5]).getText());
-                int height = Integer.parseInt(((JTextField) components[7]).getText());
-                return new SVGRectangle(x, y, width, height, Color.BLACK, 1.0, Color.BLACK, 1.0, 1.0);
-            case "Circle":
-                int centerXCircle = Integer.parseInt(((JTextField) components[1]).getText());
-                int centerYCircle = Integer.parseInt(((JTextField) components[3]).getText());
-                int radius = Integer.parseInt(((JTextField) components[5]).getText());
-                return new SVGCircle(centerXCircle, centerYCircle, radius, Color.BLACK, 1.0, Color.BLACK, 1.0, 1.0);
-            case "Ellipse":
-                int centerXEll = Integer.parseInt(((JTextField) components[1]).getText());
-                int centerYEll = Integer.parseInt(((JTextField) components[3]).getText());
-                int widthEll = Integer.parseInt(((JTextField) components[5]).getText());
-                int heightEll = Integer.parseInt(((JTextField) components[7]).getText());
-                return new SVGEllipse(centerXEll, centerYEll, widthEll, heightEll, Color.BLACK, 1.0, Color.BLACK, 1.0,
-                        1.0);
-            case "Line":
-                int startX = Integer.parseInt(((JTextField) components[1]).getText());
-                int startY = Integer.parseInt(((JTextField) components[3]).getText());
-                int endX = Integer.parseInt(((JTextField) components[5]).getText());
-                int endY = Integer.parseInt(((JTextField) components[7]).getText());
-                return new SVGLine(startX, startY, endX, endY, Color.BLACK, 1.0, 1.0);
-            default:
-                return null;
+        try {
+            switch (shapeType) {
+                case "Rectangle":
+                    x = getIntValueFromComponent(components, X_LABEL);
+                    y = getIntValueFromComponent(components, Y_LABEL);
+                    width = getIntValueFromComponent(components, WIDTH_LABEL);
+                    height = getIntValueFromComponent(components, HEIGHT_LABEL);
+                    return new SVGRectangle(x, y, width, height, Color.BLACK, 1.0, Color.BLACK, 1.0, 1.0);
+                case "Circle":
+                    centerX = getIntValueFromComponent(components, CENTER_X_LABEL);
+                    centerY = getIntValueFromComponent(components, CENTER_Y_LABEL);
+                    radius = getIntValueFromComponent(components, RADIUS_LABEL);
+                    return new SVGCircle(centerX, centerY, radius, Color.BLACK, 1.0, Color.BLACK, 1.0, 1.0);
+                case "Ellipse":
+                    centerX = getIntValueFromComponent(components, CENTER_X_LABEL);
+                    centerY = getIntValueFromComponent(components, CENTER_Y_LABEL);
+                    width = getIntValueFromComponent(components, WIDTH_LABEL);
+                    height = getIntValueFromComponent(components, HEIGHT_LABEL);
+                    return new SVGEllipse(centerX, centerY, width, height, Color.BLACK, 1.0, Color.BLACK, 1.0, 1.0);
+                case "Line":
+                    startX = getIntValueFromComponent(components, START_X_LABEL);
+                    startY = getIntValueFromComponent(components, START_Y_LABEL);
+                    endX = getIntValueFromComponent(components, END_X_LABEL);
+                    endY = getIntValueFromComponent(components, END_Y_LABEL);
+                    return new SVGLine(startX, startY, endX, endY, Color.BLACK, 1.0, 1.0);
+                default:
+                    return null;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(panel, "Invalid input. Please enter valid numbers.", "Error", JOptionPane.ERROR_MESSAGE);
+            return null;
         }
     }
 
+    private static int getIntValueFromComponent(Component[] components, String label) {
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] instanceof JLabel && ((JLabel) components[i]).getText().equals(label)) {
+                JTextField textField = (JTextField) components[i + 1];
+                return Integer.parseInt(textField.getText());
+            }
+        }
+        throw new IllegalArgumentException("Label not found: " + label);
+    }
+
+    
     public static SVGRectangle showRectangleDialog(Component parent, String title) {
-        JPanel panel = createShapePanel(new String[] { "X:", "Y:", "Width:", "Height:" });
+        JPanel panel = createShapePanel(new String[] { X_LABEL, Y_LABEL, WIDTH_LABEL, HEIGHT_LABEL });
         return (SVGRectangle) showShapeDialog(parent, title, panel, "Rectangle");
     }
 
+    
     public static SVGCircle showCircleDialog(Component parent, String title) {
-        JPanel panel = createShapePanel(new String[] { "Center X:", "Center Y:", "Radius:" });
+        JPanel panel = createShapePanel(new String[] { CENTER_X_LABEL, CENTER_Y_LABEL, RADIUS_LABEL });
         return (SVGCircle) showShapeDialog(parent, title, panel, "Circle");
     }
 
-    public static SVGEllipse showEllipseDialog(Component parent, String title) {
-        JPanel panel = createShapePanel(new String[] { "Center X:", "Center Y:", "Width:", "Height:" });
+   
+   public static SVGEllipse showEllipseDialog(Component parent, String title) {
+        JPanel panel = createShapePanel(new String[] { CENTER_X_LABEL, CENTER_Y_LABEL, WIDTH_LABEL, HEIGHT_LABEL });
         return (SVGEllipse) showShapeDialog(parent, title, panel, "Ellipse");
     }
 
-    public static SVGLine showLineDialog(Component parent, String title) {
-        JPanel panel = createShapePanel(new String[] { "Start X:", "Start Y:", "End X:", "End Y:" });
+    
+   public static SVGLine showLineDialog(Component parent, String title) {
+        JPanel panel = createShapePanel(new String[] { START_X_LABEL, START_Y_LABEL, END_X_LABEL, END_Y_LABEL });
         return (SVGLine) showShapeDialog(parent, title, panel, "Line");
     }
 
--- src/test/java/es/upm/pproject/geditor/model/SVGGroupTest.java
+++ src/test/java/es/upm/pproject/geditor/model/SVGGroupTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import es.upm.pproject.geditor.model.SVGGroup;
 import es.upm.pproject.geditor.model.SVGRectangle;
 
-public class SVGGroupTest {
+class SVGGroupTest {
 
     @Test
     void testAddElement() {
